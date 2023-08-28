4e99fd589a64d7142d90da63b494423a74c4a317
Martin Deinhofer <deinhofer.m@gmail.com>
2023-08-28 23:27:30 +0200

    added change mode: if z acceleration is -1000 and button is pressed emulation mode is changed

--- src/keyboard-space-mouse-hold-left-combined.js
+++ src/keyboard-space-mouse-hold-left-combined.js
@@ -2,21 +2,36 @@ var int = require("ble_hid_combo");
 NRF.setServices(undefined, { hid : int.report });
 
 let modeIndex=1;
-const modeValue=[int.BUTTON.LEFT, 44];
+const modeValue=[int.BUTTON.LEFT, 44, 40];
+let changeMode=0;
 
-function btnPressed() {
-  //int.scroll(10);         // Scroll down
-  //int.moveMouse(30, 0);   // Move mouse horizontally
-  //int.tapKey(int.KEY.Y);  // Also press the Y key
-  //NRF.sendHIDReport([0,0,44], function() {
-  //  NRF.sendHIDReport([0,0,0]);
-  //});
-  if(modeIndex==0) {
-    int.holdButton(modeValue[modeIndex]);
-  } else if(modeIndex==1) {
-    int.keyDown(modeValue[modeIndex]);
+//Puck.accelOn(); // default is 12.5Hz, with gyro
+Puck.accelOn(1.6); //for 1.6Hz low power, without gyro
+Puck.on('accel', function(a) {
+  //console.log(a);
+  if(a.acc.z < -1000) {
+    changeMode=1;
+  } else {
+    changeMode=0;
   }
-}
+});
+
+function btnPressed() { 
+    if(changeMode==0) {
+      if(modeIndex==0) {
+        int.holdButton(modeValue[modeIndex]);
+      } else {
+        int.keyDown(modeValue[modeIndex]);
+      }
+    } else {
+      modeIndex=modeIndex+1;
+      if(modeIndex==modeValue.length) {
+        modeIndex=0;
+      }
+      console.log("modeIndex changed: "+modeIndex);
+      digitalPulse(LED1,1,200*(modeIndex+1));
+    }
+  }
 
 function btnReleased(){
   int.releaseButton(int.BUTTON.ALL, function() {
